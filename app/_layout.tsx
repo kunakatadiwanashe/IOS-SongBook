657aae91da6a1f588437535c63c9b9e462285714
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2024-08-15 14:43:20 +0200

    categories

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, NavigationContainer, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, Tabs } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {StatusBar} from 'react-native';
 
 import OnboardScreen from './screens/onboardScreen';
+import BottomNavigator from './navigation/BottomNavigation';
 import HomeScreen from './screens/HomeScreen';
-import BottomNavigator  from './navigation/BottomNavigation'
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="BoardScreen" component={OnboardScreen} />
-        <Stack.Screen name="Home" component={BottomNavigator} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+   <Stack>
+    <Stack.Screen name='(tabs)' options={{headerShown: false}} />
+   </Stack>
+ 
   );
+
+
 }
---
+++ assets/consts/categories.tsx
@@ -0,0 +1,8 @@
+const categories = [
+{id: '1', name: 'Praise', image: require('../images/icon.png')},
+{id: '2', name: 'Worship', image: require('../images/icon.png')},
+{id: '3', name: 'Prayer', image: require('../images/icon.png')},
+{id: '4', name: 'Band', image: require('../images/icon.png')},
+];
+
+export default categories
